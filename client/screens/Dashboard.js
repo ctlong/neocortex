ef5eef9c2cff5568dc6aec7b8c2810bf4f16b876
Wilhelm Willie <wilhelmwillie@gmail.com>
2019-04-23 16:16:15 -0700

    Added recent interactions to dashboard screen

--- client/screens/Dashboard.js
+++ client/screens/Dashboard.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { StyleSheet, Text, View, Button } from 'react-native'
+import { StyleSheet, Text, View, Button, Image, ScrollView } from 'react-native'
 
 import Header from '../components/Dashboard/Header'
 
@@ -12,6 +12,23 @@ export default class Dashboard extends React.Component {
     return (
       <View style={styles.container}>
         <Header />
+
+        <ScrollView>
+          <View style={styles.contentContainer}>
+            <Text style={styles.recentInteractions}>Recent Interactions</Text>
+
+            <View style={styles.interactionItem}>
+              <View>
+                <Image source={require('../assets/temp_pic.png')} style={styles.interactionImage} />
+              </View>
+
+              <View>
+                <Text style={styles.interactionName}>Jennifer Rodham</Text>
+                <Text style={styles.interactionDetail}>Meeting | April 14, 11:20 AM</Text>
+              </View>
+            </View>
+          </View>
+        </ScrollView>
       </View>
     )
   }
@@ -20,11 +37,34 @@ export default class Dashboard extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#EDF1F2',
   },
-  add: {
-    position: 'absolute',
-    bottom: 25,
-    right: 25
+  recentInteractions: {
+    fontSize: 30,
+    fontWeight: 'bold',
+    color: '#585858'
+  },
+  contentContainer: {
+    padding: 25
+  },
+  interactionItem: {
+    padding: 25,
+    backgroundColor: '#fff',
+    marginTop: 25,
+    flexDirection: 'row',
+    alignItems: 'center'
+  },
+  interactionImage: {
+    width: 60,
+    height: 60,
+    marginRight: 15
+  },
+  interactionName: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    paddingBottom: 5
+  },
+  interactionDetail: {
+    fontSize: 16
   }
 })
