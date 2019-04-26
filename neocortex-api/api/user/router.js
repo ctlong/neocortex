f4312e6680b6b86c4b0f1640ff013f33977af893
Carson Long <12767276+ctlong@users.noreply.github.com>
2019-04-26 15:16:04 -0700

    init/backend: more sanitization for user inputs

--- neocortex-api/api/user/router.js
+++ neocortex-api/api/user/router.js
@@ -7,12 +7,15 @@ const router = express.Router();
 
 router.route('/login')
   .post([
-      check('email').isEmail()
+      check('email').isEmail().normalizeEmail(),
+      check('password').length({min: 8}).trim().escape()
     ], User.login)
 
 router.route('/register')
   .post([
-      check('email').isEmail()
+      check('email').isEmail().normalizeEmail(),
+      check('password').length({min: 8}).trim().escape(),
+      check('name').isLength({min: 1}).trim().escape()
     ], User.register)
 
 module.exports = router;
